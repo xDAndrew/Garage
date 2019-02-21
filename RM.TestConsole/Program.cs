35663cbdbd36f21824298b8bfe126e11f6ab2367
xDAndrew <xD.Andrew2012@yandex.by>
2019-02-21 20:01:36 +0300

    WorkOff Document complited

--- RM.TestConsole/Program.cs
+++ RM.TestConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using RepairsManager.WriteOffModule.Models;
 using RepairsManager.WriteOffModule.Services;
 
 namespace RM.TestConsole
@@ -8,7 +10,18 @@ namespace RM.TestConsole
     {
         static void Main(string[] args)
         {
-            File.WriteAllBytes(@"Price1.xlsx", StateRepository.GetWorkOffCertificate(null, DateTime.Now));
+            var repaires = new List<Repair>()
+            {
+                new Repair() { Detail = "Автолампа А 12-5 одноконтактная", Party = "", Price = 0.50m, Unit = "шт", Reason = "Использована"},
+                new Repair() { Detail = "Автолампа А 12-5 без цоколя", Party = "", Price = 0.25m, Unit = "шт", Reason = "Использована на авто" },
+                new Repair() { Detail = "Блок контрольных ламп УАЗ Хантер правый (43.3803) 3151-20-3803010", Party = "", Price = 13.28m, Unit = "шт", Reason = "Использована" },
+                new Repair() { Detail = "Втулка маятника", Party = "", Price = 0.03m, Unit = "шт", Reason = "Использована" },
+                new Repair() { Detail = "Гидротолкатель (406-409дв) 8шт 406-1007045-51", Party = "", Price = 66.50m, Unit = "компл", Reason = "Использована" },
+                new Repair() { Detail = "Колодка тормоза зад.длин.", Party = "", Price = 29.67m, Unit = "шт", Reason = "Использована" },
+                new Repair() { Detail = "Лампа Н4 LL, 12V, 60/55 W", Party = "", Price = 15.50m, Unit = "шт", Reason = "Использована" },
+                new Repair() { Detail = "Лента щетки стеклоочистителя ГАЗ-3302/2108 3302-5205900", Party = "", Price = 3.20m, Unit = "шт", Reason = "Использована" },
+            };
+            File.WriteAllBytes(@"Price1.xlsx", StateRepository.GetWorkOffCertificate(repaires, DateTime.Now));
             Console.WriteLine(StateRepository.State.Director);
         }
     }
--- RepairsManager.WriteOffModule/Services/StateRepository.cs
+++ RepairsManager.WriteOffModule/Services/StateRepository.cs
@@ -16,6 +16,13 @@ namespace RepairsManager.WriteOffModule.Services
     {
         public static byte[] GetWorkOffCertificate(IEnumerable<Repair> repairs, DateTime createDate)
         {
+            if (repairs == null)
+                throw new NullReferenceException();
+
+            var commissionStartRow = 16;
+            var materialsStartRow = 19;
+            var endRowStart = 23;
+
             var currentState = State;
 
             var templateFileInfo = new FileInfo(@"WorkOffTemplate.xlsx");
@@ -32,7 +39,7 @@ namespace RepairsManager.WriteOffModule.Services
                 sheet.Cells[13, 5].Value = currentState.Commission.Chairman.Place;
                 sheet.Cells[13, 12].Value = currentState.Commission.Chairman.Name;
 
-                var commissionStartRow = 16;
+                
                 sheet.InsertRow(commissionStartRow, currentState.Commission.Members.Length * 2);
 
                 foreach (var item in currentState.Commission.Members)
@@ -53,7 +60,148 @@ namespace RepairsManager.WriteOffModule.Services
                     sheet.Cells[commissionStartRow, 12].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
 
                     commissionStartRow += 2;
+                    materialsStartRow += 2;
+                    endRowStart += 2;
                 }
+
+                sheet.InsertRow(materialsStartRow, repairs.Count() + 1);
+                var index = 1;
+
+                foreach (var itemRepair in repairs)
+                {
+                    sheet.Row(materialsStartRow).Style.Font.SetFromFont(new Font("Times New Roman", 12));
+                    sheet.Row(materialsStartRow).Height = 40;
+
+                    var cells = new List<ExcelRange>
+                    {
+                        sheet.Cells[materialsStartRow, 1, materialsStartRow, 2],
+                        sheet.Cells[materialsStartRow, 3, materialsStartRow, 6],
+                        sheet.Cells[materialsStartRow, 7, materialsStartRow, 7],
+                        sheet.Cells[materialsStartRow, 8, materialsStartRow, 10],
+                        sheet.Cells[materialsStartRow, 11, materialsStartRow, 11],
+                        sheet.Cells[materialsStartRow, 12, materialsStartRow, 14],
+                        sheet.Cells[materialsStartRow, 15, materialsStartRow, 18],
+                        sheet.Cells[materialsStartRow, 19, materialsStartRow, 20]
+                    };
+
+                    foreach (var itemCell in cells)
+                    {
+                        itemCell.Merge = true;
+                        itemCell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        itemCell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                        itemCell.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                        itemCell.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                        itemCell.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                        itemCell.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                        itemCell.Style.WrapText = true;
+                    }
+
+                    cells[0].Value = index;
+                    cells[1].Value = itemRepair.Detail;
+                    cells[2].Value = itemRepair.Unit;
+                    cells[3].Value = itemRepair.Party;
+                    cells[4].Value = 1.00m;
+                    cells[5].Value = itemRepair.Price;
+                    cells[6].Value = itemRepair.Price;
+                    cells[7].Value = itemRepair.Reason;
+
+                    cells[4].Style.Numberformat.Format = "0.00";
+                    cells[5].Style.Numberformat.Format = "0.00";
+                    cells[6].Style.Numberformat.Format = "0.00";
+
+                    index++;
+                    materialsStartRow++;
+                    endRowStart++;
+                }
+
+                //summary
+                sheet.Row(materialsStartRow).Style.Font.SetFromFont(new Font("Times New Roman", 12));
+                sheet.Row(materialsStartRow).Height = 40;
+
+                var tempList = new List<ExcelRange>
+                {
+                    sheet.Cells[materialsStartRow, 1, materialsStartRow, 6],
+                    sheet.Cells[materialsStartRow, 7, materialsStartRow, 7],
+                    sheet.Cells[materialsStartRow, 8, materialsStartRow, 10],
+                    sheet.Cells[materialsStartRow, 11, materialsStartRow, 11],
+                    sheet.Cells[materialsStartRow, 12, materialsStartRow, 14],
+                    sheet.Cells[materialsStartRow, 15, materialsStartRow, 18],
+                    sheet.Cells[materialsStartRow, 19, materialsStartRow, 20]
+                };
+
+                foreach (var itemCell in tempList)
+                {
+                    itemCell.Merge = true;
+                    itemCell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    itemCell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                    itemCell.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                    itemCell.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                    itemCell.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                    itemCell.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                    itemCell.Style.WrapText = true;
+                }
+
+                tempList[0].Style.Font.Bold = true;
+                tempList[0].Value = "Итого";
+                tempList[1].Value = "X";
+                tempList[3].FormulaR1C1 = $"SUM(R[-{index - 1}]C:R[-1]C)";
+                tempList[4].Value = "X";
+                tempList[5].FormulaR1C1 = $"SUM(R[-{index - 1}]C:R[-1]C[3])";
+                tempList[6].Value = "X";
+
+                tempList[3].Style.Numberformat.Format = "0.00";
+                tempList[5].Style.Numberformat.Format = "0.00";
+
+                endRowStart++;
+                //end summary
+
+                sheet.Cells[endRowStart, 4].Value = currentState.Commission.Chairman.Place;
+                sheet.Cells[endRowStart, 14].Value = currentState.Commission.Chairman.Name;
+                endRowStart += 3;
+
+                foreach (var item in currentState.Commission.Members)
+                {
+                    sheet.Row(endRowStart).Style.Font.SetFromFont(new Font("Arial", 11));
+                    sheet.Row(endRowStart).Height = 20;
+
+                    sheet.Row(endRowStart + 1).Height = 10;
+                    sheet.Row(endRowStart + 1).Style.Font.SetFromFont(new Font("Arial", 7));
+
+                    sheet.Row(endRowStart + 2).Height = 10;
+
+                    sheet.Cells[endRowStart, 4, endRowStart, 6].Merge = true;
+                    sheet.Cells[endRowStart + 1, 4, endRowStart + 1, 6].Merge = true;
+                    sheet.Cells[endRowStart, 14, endRowStart, 18].Merge = true;
+                    sheet.Cells[endRowStart + 1, 14, endRowStart + 1, 18].Merge = true;
+                    sheet.Cells[endRowStart, 10, endRowStart, 11].Merge = true;
+                    sheet.Cells[endRowStart + 1, 10, endRowStart + 1, 11].Merge = true;
+
+                    sheet.Cells[endRowStart, 4].Value = item.Place;
+                    sheet.Cells[endRowStart + 1, 4].Value = "(должность)";
+                    sheet.Cells[endRowStart, 4, endRowStart, 6].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                    sheet.Cells[endRowStart, 4].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    sheet.Cells[endRowStart + 1, 4].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+                    sheet.Cells[endRowStart + 1, 10].Value = "(подпись)";
+                    sheet.Cells[endRowStart, 10, endRowStart, 11].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                    sheet.Cells[endRowStart, 10].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    sheet.Cells[endRowStart + 1, 10].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+                    sheet.Cells[endRowStart, 14].Value = item.Name;
+                    sheet.Cells[endRowStart + 1, 14].Value = "(И.О.Фамилия)";
+                    sheet.Cells[endRowStart, 14, endRowStart, 18].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                    sheet.Cells[endRowStart, 14].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    sheet.Cells[endRowStart + 1, 14].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+                    endRowStart += 3;
+                }
+
+                endRowStart -= currentState.Commission.Members.Length * 3;
+                sheet.Cells[endRowStart, 1, endRowStart, 3].Style.Font.SetFromFont(new Font("Arial", 9));
+                sheet.Cells[endRowStart, 1, endRowStart, 3].Style.Font.Bold = true;
+                sheet.Cells[endRowStart, 1, endRowStart, 3].Merge = true;
+                sheet.Cells[endRowStart, 1, endRowStart, 3].Value = "Члены коммиссии:";
+                sheet.Cells[endRowStart, 1, endRowStart, 3].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
 
                 return eP.GetAsByteArray();
             }

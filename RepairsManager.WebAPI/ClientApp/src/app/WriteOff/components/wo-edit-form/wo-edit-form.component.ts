042ac1f334df4d0d088bedb8dc7ac1fb254cb5c2
xDAndrew <xD.Andrew2012@yandex.by>
2019-02-28 15:43:46 +0300

    Implementing

--- RepairsManager.WebAPI/ClientApp/src/app/WriteOff/components/wo-edit-form/wo-edit-form.component.ts
+++ RepairsManager.WebAPI/ClientApp/src/app/WriteOff/components/wo-edit-form/wo-edit-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WriteOffApiService } from '../../services/api/write-off-api.service';
-import { State } from '../../models/state';
+import { State, Member } from '../../models/state';
 
 @Component({
   selector: 'app-wo-edit-form',
@@ -11,15 +11,50 @@ export class WoEditFormComponent implements OnInit {
 
   public model: State;
   public period: Date = new Date();
+  public loaded: boolean = false;
 
-  constructor(private api:WriteOffApiService) { }
+  constructor(private api: WriteOffApiService) { }
 
   ngOnInit() {
-    this.api.getWriteOffState().subscribe((data) => {this.model = data});
+    this.api.getWriteOffState().subscribe((data) => {
+      this.model = data;
+      this.model.commission.createDate = new Date(this.model.commission.createDate);
+      this.loaded = true;
+    });
   }
 
   getDocument() {
     this.api.getDocument(this.period);
   }
 
+  removeEmployee(index: number) {
+    this.model.commission.members.splice(index, 1);
+  }
+
+  addNewEmployee() {
+    this.model.commission.members.push(new Member());
+  }
+
+  saveModel() {
+    this.api.putWriteOffState(this.model).subscribe();
+  }
+
+  getSaveValidation() {
+    let result: boolean =
+      !!this.model.organization &&
+      !!this.model.director &&
+      !!this.model.department &&
+      !!this.model.commission.members.length &&
+      !!this.model.commission.chairman &&
+      !!this.model.commission.createDate &&
+      !!this.model.commission.number;
+    this.model.commission.members.forEach((elenet) => {
+      result = result && !!elenet.name && !!elenet.place;
+    });
+    return !result;
+  }
+
+  getValidation() {
+    return !this.period;
+  }
 }
--- RepairsManager.WebAPI/ClientApp/src/app/WriteOff/writeoff.module.ts
+++ RepairsManager.WebAPI/ClientApp/src/app/WriteOff/writeoff.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { WoEditFormComponent } from './components/wo-edit-form/wo-edit-form.component';
 import { CalendarModule } from 'primeng/calendar';
 import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { FieldsetModule } from 'primeng/fieldset';
 
 @NgModule({
   declarations: [WoEditFormComponent],
   imports: [
     CommonModule,
     CalendarModule,
-    FormsModule
+    FormsModule,
+    InputTextModule,
+    FieldsetModule
   ],
   exports: [
     WoEditFormComponent
